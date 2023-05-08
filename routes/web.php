27263111b5d7be6738c2a969ef4d6f1648e19a65
tttpiano <dragonno005@gmail.com>
2023-05-08 10:31:04 +0700

    create new router

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ShopController;
+use App\Http\Controllers\ShopDetailController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomAuthController;
+use App\Http\Controllers\ProductController;
+
+Route::get('dashboard', [CustomAuthController::class, 'dashboard']);
+Route::get('login', [CustomAuthController::class, 'index'])->name('login');
+Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom');
+Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
+Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
+Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
+Route::get('/users/{id}', [CustomAuthController::class, 'show'])->name('show');
+Route::get('pages', [CustomAuthController::class, 'phanTrang'])->name('abc');
+
+
+Route::get('/shop', [ShopController::class, 'show'])->name('shop');
+
+
